e90a84d32736c6964bc81a346f972c97a037ca69
刘应铎 <5416367+liu_ying_duo@user.noreply.gitee.com>
2025-03-19 17:24:09 +0800

    修改报错

--- machine_learning/get_stock_details.py
+++ machine_learning/get_stock_details.py
@@ -1,4 +1,6 @@
 import akshare as ak
 
 stock_individual_basic_info_xq_df = ak.stock_individual_basic_info_xq(symbol="SH601127",token="4070a625ad532428a076ebb053b345d0e3fda53a")
-print(stock_individual_basic_info_xq_df)
+#根据item列和value列生成字典
+stock_individual_basic_info_xq_dict = dict(zip(stock_individual_basic_info_xq_df['item'],stock_individual_basic_info_xq_df['value']))
+print(stock_individual_basic_info_xq_dict["main_operation_business"])
